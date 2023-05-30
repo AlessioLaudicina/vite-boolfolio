3fc9cf238b7d79b6a300c4b1ebed5a1734f6cc32
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-05-30 16:54:52 +0200

    prova risposta axios

---
+++ src/components/AppMain.vue
@@ -0,0 +1,35 @@
+<script>
+
+import axios from 'axios';
+export default{
+    name: 'AppMain',
+    data(){
+        return {
+
+        }
+    },
+    methods: {
+        getPosts(){
+            console.log('Forza inter')
+            axios.get('http://localhost:8000/api/posts')
+            .then(response => {
+                console.log(response)
+            })
+
+        }
+
+    },
+    mounted(){
+        this.getPosts();
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+
+</template>
